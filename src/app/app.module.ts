042fc3f7c371fc0486f318de33227cf108674325
meleromero <milagrosrrr9@gmail.com>
2022-10-31 16:10:06 -0300

    se agrega proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+import { FormsModule } from '@angular/forms';
+
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AboutComponent,
+    ExperienciaComponent,
+    HysComponent,
+    ProyectsComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule,
+    
+    
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/nuevo-usuario.ts
@@ -0,0 +1,7 @@
+export interface NuevoUsuario {
+    nombre: String;
+    nombreUsuario: String;
+    email: String;
+    password: String;
+    authorities: String[];
+}
